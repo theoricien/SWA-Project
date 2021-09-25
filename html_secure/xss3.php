8e1d1a6b288e861ddeed8376c67987c7027fa152
theoricien <letheoricien@hotmail.com>
2021-09-25 22:01:04 +0200

    (#4) XSS Rule 3

---
+++ html_secure/xss3.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title> XSS Rule 3 </title>
+    </head>
+
+    <body>
+        <h1> XSS Rule 3 </h1>
+
+        <form method="get" action=<?php echo "\"" . $_SERVER['PHP_SELF'] . "\""; ?>>
+            <label for="untrusted"> Donne une valeur a la variable x </label>
+            <input type="number" name="untrusted">
+            <input type="submit" value="Submit">
+        </form>
+
+        <?php
+            if (isset($_GET["untrusted"]))
+            {
+                echo '<script> x=' . json_encode($_GET["untrusted"]) . '; console.log(x); </script>';
+                echo 'Regarde ta console pour voir le résultat !';
+            }
+        ?>
+    </body>
+</html>
