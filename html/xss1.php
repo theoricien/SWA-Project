4756812881e5face46691cfbce43f60bc24ac4ce
theoricien <letheoricien@hotmail.com>
2021-09-25 22:00:45 +0200

    (#2) XSS Rule 1

---
+++ html/xss1.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title> XSS Rule 1 </title>
+    </head>
+
+    <body>
+        <h1> XSS Rule 1 </h1>
+
+        <form method="get" action=<?php echo "\"" . $_SERVER['PHP_SELF'] . "\""; ?>>
+            <label for="untrusted"> Quel est ton nom ? </label>
+            <input type="text" name="untrusted">
+            <input type="submit" value="Submit">
+        </form>
+
+        <?php
+            if (isset($_GET["untrusted"]) &&
+                !empty($_GET["untrusted"]))
+            {
+                echo "<p> Salut " .$_GET["untrusted"] . " ! </p>";
+            }
+        ?>
+    </body>
+</html>
