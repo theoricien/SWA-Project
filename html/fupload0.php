820014df29511557ee72bfeae88a6bb4e7ffcc7b
theoricien <letheoricien@hotmail.com>
2021-10-03 15:10:52 +0200

    (#20) Unrestricted File Upload

---
+++ html/fupload0.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title> File Upload 0 </title>
+    </head>
+
+    <body>
+        <h1> File Upload 0 </h1>
+
+        <form method="post" enctype="multipart/form-data" action=<?php echo "\"" . $_SERVER['PHP_SELF'] . "\""; ?>>
+            <input type="hidden" name="MAX_FILE_SIZE" value="31457280" />
+            <input type="file" name="untrusted">
+            <input type="submit">
+        </form>
+
+        <?php
+            $uploads_dir =  __DIR__ . "/uploads/";
+            if (isset($_FILES["untrusted"]))
+            {
+                $file = $_FILES["untrusted"];
+                if ($file["error"] === UPLOAD_ERR_OK)
+                {
+                    $success = move_uploaded_file($file["tmp_name"], $uploads_dir . $file["name"]);
+                    if ($success)
+                    {
+                        echo "uploaded in " . $uploads_dir . $file["name"];
+                    } else {
+                        echo "upload fails.";
+                    }    
+                } else 
+                {
+                    echo "there is a problem with the upload: " . $file["error"];
+                }
+            } 
+        ?>
+    </body>
+</html>
---
+++ html_secure/fupload0.php
@@ -0,0 +1,57 @@
+<html>
+    <head>
+        <title> File Upload </title>
+    </head>
+
+    <body>
+        <h1> File Upload </h1>
+        <p>We only accept PNG and JPG files</p>
+
+        <form method="post" enctype="multipart/form-data" action=<?php echo "\"" . $_SERVER['PHP_SELF'] . "\""; ?>>
+            <input type="hidden" name="MAX_FILE_SIZE" value="31457280" />
+            <input type="file" name="untrusted">
+            <input type="submit">
+        </form>
+
+        <?php
+            if (!isset($_FILES["untrusted"])) {
+                die("There is no file to upload.");
+            }
+
+            $filepath = $_FILES['untrusted']['tmp_name'];
+            $fileSize = filesize($filepath);
+            $fileinfo = finfo_open(FILEINFO_MIME_TYPE);
+            $filetype = finfo_file($fileinfo, $filepath);
+
+            if ($fileSize === 0) {
+                die("The file is empty.");
+            }
+
+            if ($fileSize > 3145728) { // 3 MB (1 byte * 1024 * 1024 * 3 (for 3 MB))
+                die("The file is too large");
+            }
+
+            $allowedTypes = [
+                'image/png' => 'png',
+                'image/jpeg' => 'jpg'
+            ];
+
+            if (!in_array($filetype, array_keys($allowedTypes))) {
+                die("File not allowed.");
+            }
+
+            $filename = basename($filepath); // I'm using the original name here, but you can also change the name of the file here
+            $extension = $allowedTypes[$filetype];
+            $targetDirectory = __DIR__ . "/uploads"; // __DIR__ is the directory of the current PHP file
+
+            $newFilepath = $targetDirectory . "/" . $filename . "." . $extension;
+
+            if (!copy($filepath, $newFilepath)) { // Copy the file, returns false if failed
+                die("Can't move file.");
+            }
+            unlink($filepath); // Delete the temp file
+
+            echo "File uploaded successfully :)";
+        ?>
+    </body>
+</html>
